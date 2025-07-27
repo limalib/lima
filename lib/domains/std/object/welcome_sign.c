1da888b2d5bd91d8060c3ceeba27432ffb96c605
Tsathoggua of NKai <sstaunp+github@gmail.com>
2025-07-27 18:42:46 +0200

    fix:removed @ENDMARKER syntax

--- lib/domains/std/object/welcome_sign.c
+++ lib/domains/std/object/welcome_sign.c
@@ -7,16 +7,12 @@ void setup()
    set_adj("small", "weathered");
    set_in_room_desc("A small sign is here, trenched deeply in the sand.");
    set_long("The sign is quite weathered, but still readable, nonetheless.");
-    set_text( @ENDMARKER
-
-Warning:
-
-DON'T USE THIS AREA ON YOUR MUD!!!
-
-This area is part of every LIMA based MUD out there. We are providing this area as an example only!  You can use the code to the extent that you want to code something similar by example, but otherwise, get your own ideas =P
-
-As for this area, if you want to play it before you read the code, there are nine rooms and a treasure to find.  See if you can do it...
-
--- The LIMA guys.
-ENDMARKER );
+   set_text("Warning:\n\n"
+            "DON'T USE THIS AREA ON YOUR MUD!!!\n\n"
+            "This area is part of every LIMA based MUD out there. We are providing this area as an example only!  You "
+            "can use the code to the extent that you want to code something similar by example, but otherwise, get "
+            "your own ideas =P\n\n"
+            "As for this area, if you want to play it before you read the code, there are nine rooms and a treasure to "
+            "find.  See if you can do it...\n\n"
+            "-- The LIMA guys.\n");
 }
